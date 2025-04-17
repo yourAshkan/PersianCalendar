a02821f1a2e0468c0d419448ccc84ebc2e80c481
yourAshkan <ashkan.kiya11@gmail.com>
2025-04-17 13:04:32 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Train;
+try
+{
+	Console.Write("Please Enter a DateTime : ");
+	DateTime date = DateTime.Parse(Console.ReadLine());
+	Console.WriteLine($"Your Time Changed to the Persian Calendar : {Tools.PersianDate(date)}");
+}
+catch (FormatException)
+{
+	Console.WriteLine("Invalid Input!");
+}
+Console.ReadKey();
+
+
---
+++ Tools.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Train
+{
+    public static class Tools
+    {
+        public static string PersianDate(this DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            var year = pc.GetYear(date);
+            var month = pc.GetMonth(date);
+            var day = pc.GetDayOfMonth(date);
+
+            return $"{year}/{month}/{day}";
+        }
+    }
+}
